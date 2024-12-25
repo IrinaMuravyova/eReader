5cd7e391493759a1a5d0f9f77951e8569b92e9e7
IrinaMuravyova <miomir@yandex.ru>
2024-12-25 20:42:26 +0800

    Added structure of UI (section with collectionView)

---
+++ eReader/Cells/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  eReader
+//
+//  Created by Irina Muravyeva on 25.12.2024.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ eReader/Cells/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  eReader
+//
+//  Created by Irina Muravyeva on 25.12.2024.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
